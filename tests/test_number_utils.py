b2e08f2a2bc433e9c2386378a3aca2984b1ac9b4
Por-045 <6210110045@psu.ac.th>
2022-08-08 15:47:35 +0700

    test_give_5_should_buzz

--- tests/test_number_utils.py
+++ tests/test_number_utils.py
@@ -2,13 +2,12 @@ from coe_number.number_utils import is_fizz_buzz
 
 import unittest
 
-mylist = [0, 1, 2]
-
 class FizzBuzzTest(unittest.TestCase):
-    def test_give_3_should_fizz(self):
-        number = 3
-        excepted_result = 'Fizz'
-
+    def test_give_5_should_buzz(self):
+        #arrange
+        number = 5
+        excepted_result = 'Buzz' 
+        #act
         result = is_fizz_buzz(number) 
-
+        #assert
         self.assertEqual(result, excepted_result)
