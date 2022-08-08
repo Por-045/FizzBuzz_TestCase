0abe61dc7421038a6f21880fe1b059a9820a97e7
Por-045 <6210110045@psu.ac.th>
2022-08-08 15:52:37 +0700

    3 case

--- tests/test_number_utils.py
+++ tests/test_number_utils.py
@@ -3,6 +3,24 @@ from coe_number.number_utils import is_fizz_buzz
 import unittest
 
 class FizzBuzzTest(unittest.TestCase):
+    def test_give_3_should_fizz(self):
+        #arrange
+        number = 3
+        excepted_result = 'Fizz' 
+        #act
+        result = is_fizz_buzz(number) 
+        #assert
+        self.assertEqual(result, excepted_result)
+
+    def test_give_5_should_buzz(self):
+        #arrange
+        number = 5
+        excepted_result = 'Buzz' 
+        #act
+        result = is_fizz_buzz(number) 
+        #assert
+        self.assertEqual(result, excepted_result)
+    
     def test_give_15_should_fizz_buzz(self):
         #arrange
         number = 15
