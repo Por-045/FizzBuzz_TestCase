a7fae520f501bf0ad0f425ebc11a20993a03b398
Por-045 <6210110045@psu.ac.th>
2022-08-08 15:45:20 +0700

    test_give_3_should_fizz

--- coe_number/number_utils.py
+++ coe_number/number_utils.py
@@ -1,8 +1,8 @@
 def is_fizz_buzz(number):
-    if (number >= 1 & number <= 1000):
-        if number % 3 == 0 & number % 5 == 0:
-            return 0
+    if (number >= 1 and number <= 1000):
+        if (number % 3 == 0) & (number % 5 == 0):
+            return 'Fizz Buzz'
         elif number % 3 == 0:
-            return 1
+            return 'Fizz'
         elif number % 5 == 0:
-            return 2
+            return 'Buzz'
--- tests/test_number_utils.py
+++ tests/test_number_utils.py
@@ -5,8 +5,10 @@ import unittest
 mylist = [0, 1, 2]
 
 class FizzBuzzTest(unittest.TestCase):
-    def test_give_3(self):
-        num = 7
-        is_result = is_fizz_buzz(num)
-        print(is_result)
-        self.assertIn(is_result, mylist)
+    def test_give_3_should_fizz(self):
+        number = 3
+        excepted_result = 'Fizz'
+
+        result = is_fizz_buzz(number) 
+
+        self.assertEqual(result, excepted_result)
